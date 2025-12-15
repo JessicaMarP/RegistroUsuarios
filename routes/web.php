0c7de613e4412d14add3b147acd930392cb5a825
JessicaMarP <L21380425@cdvictoria.tecnm.mx>
2025-12-15 11:47:52 -0600

    Feature google

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,20 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('google-auth/redirect', function(){
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('google-auth/callback', function(){
+    $user_google = Socialite::driver('google')->user();
+    $user = User::updateOrCreate([
+        'google_id' => $user_google->id,
+    ],[
+        'name' => $user_google->name,
+        'email' => $user_google->email,
+    ]);
+
+    Auth::login($user);
+    return redirect('/dashboard');
+});
